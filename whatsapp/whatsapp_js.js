3282815464e2e2aa740d0957f8e961a410f52835
vs <urt.dress@gmail.com>
2022-05-03 04:34:25 +0300

    Create whatsapp_js.js

---
+++ whatsapp/whatsapp_js.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+  //в новой структуре ответов оценки
+  $('.test').on('click', function(e) {
+
+    console.log('test');
+
+  });
+
+  console.log('test');
+
+});
